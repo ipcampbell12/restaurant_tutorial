2eea02e5c24910df4a0f90ec84ed081d713a8078
ipcampbell12 <ipcampbell12@gmail.com>
2023-05-02 09:32:40 -0700

    css changes

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,31 +1,36 @@
-import React from 'react';
-import classes from './Checkout.module.css'
+import classes from './Checkout.module.css';
 
-function Checkout(props) {
+const Checkout = (props) => {
+    const confirmHandler = (event) => {
+        event.preventDefault();
+    };
+
     return (
-        <form>
-
+        <form className={classes.form} onSubmit={confirmHandler}>
             <div className={classes.control}>
-                <label htmlFor="name">Your Name</label>
-                <input type="text" id="name" />
+                <label htmlFor='name'>Your Name</label>
+                <input type='text' id='name' />
             </div>
             <div className={classes.control}>
-                <label htmlFor="street">Street</label>
-                <input type="text" id="street" />
+                <label htmlFor='street'>Street</label>
+                <input type='text' id='street' />
             </div>
             <div className={classes.control}>
-                <label htmlFor="zipcode">Zip code</label>
-                <input type="text" id="zipcode" />
+                <label htmlFor='postal'>Postal Code</label>
+                <input type='text' id='postal' />
             </div>
             <div className={classes.control}>
-                <label htmlFor="city">City</label>
-                <input type="text" id="city" />
+                <label htmlFor='city'>City</label>
+                <input type='text' id='city' />
             </div>
-            <button type="button" onClick={props.onCancel}>Cancel</button>
-            <button>Confirm</button>
-
+            <div className={classes.actions}>
+                <button type='button' onClick={props.onCancel}>
+                    Cancel
+                </button>
+                <button className={classes.submit}>Confirm</button>
+            </div>
         </form>
     );
-}
+};
 
 export default Checkout;
