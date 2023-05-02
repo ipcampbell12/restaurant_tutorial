bb3ff293d0646d7e5177f8dda922e58af346993a
ipcampbell12 <ipcampbell12@gmail.com>
2023-05-02 09:37:36 -0700

    useRef for input

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,6 +1,15 @@
+import { useRef } from 'react';
 import classes from './Checkout.module.css';
 
+
+//useRef - don't get values with every key stroke, only get values once form is submitted
+
 const Checkout = (props) => {
+    const nameInputRef = useRef();
+    const streetInputRef = useRef();
+    const postalInputRef = useRef();
+    const cityInputRef = useRef();
+
     const confirmHandler = (event) => {
         event.preventDefault();
     };
@@ -9,19 +18,19 @@ const Checkout = (props) => {
         <form className={classes.form} onSubmit={confirmHandler}>
             <div className={classes.control}>
                 <label htmlFor='name'>Your Name</label>
-                <input type='text' id='name' />
+                <input type='text' id='name' ref={nameInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='street'>Street</label>
-                <input type='text' id='street' />
+                <input type='text' id='street' ref={streetInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='postal'>Postal Code</label>
-                <input type='text' id='postal' />
+                <input type='text' id='postal' ref={postalInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='city'>City</label>
-                <input type='text' id='city' />
+                <input type='text' id='city' ref={cityInputRef} />
             </div>
             <div className={classes.actions}>
                 <button type='button' onClick={props.onCancel}>
