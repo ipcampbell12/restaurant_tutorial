95fd2a920f7873d0006184da27b121db209e5ec2
ipcampbell12 <ipcampbell12@gmail.com>
2023-05-02 10:03:54 -0700

    conditianal formating if input is invalid

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -63,22 +63,22 @@ const Checkout = (props) => {
 
     return (
         <form className={classes.form} onSubmit={confirmHandler}>
-            <div className={classes.control}>
+            <div className={`${classes.control} ${formInputsValidity.name ? '' : classes.invalid}`}>
                 <label htmlFor='name'>Your Name</label>
                 <input type='text' id='name' ref={nameInputRef} />
                 {!formInputsValidity.name && <p> Please entere a valid name</p>}
             </div>
-            <div className={classes.control}>
+            <div className={`${classes.control} ${formInputsValidity.street ? '' : classes.invalid}`}>
                 <label htmlFor='street'>Street</label>
                 <input type='text' id='street' ref={streetInputRef} />
                 {!formInputsValidity.street && <p> Please entere a valid street</p>}
             </div>
-            <div className={classes.control}>
+            <div className={`${classes.control} ${formInputsValidity.postal ? '' : classes.invalid}`}>
                 <label htmlFor='postal'>Postal Code</label>
                 <input type='text' id='postal' ref={postalInputRef} />
-                {!formInputsValidity.postal && <p> Please entere a valid postal code</p>}
+                {!formInputsValidity.postal && <p> Please entere a valid postal code (5 digits)</p>}
             </div>
-            <div className={classes.control}>
+            <div className={`${classes.control} ${formInputsValidity.city ? '' : classes.invalid}`}>
                 <label htmlFor='city'>City</label>
                 <input type='text' id='city' ref={cityInputRef} />
                 {!formInputsValidity.city && <p> Please entere a valid city</p>}
